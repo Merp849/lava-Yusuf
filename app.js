f10f27b328037e21478315e6f7b27d78a9a235c0
Merp849 <59752534+Merp849@users.noreply.github.com>
2020-07-02 13:14:56 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,8 @@ let os = require('os');
 
 if (!child) throw new Error('Install child_process dependency dulu di package.json');
 
-//if (os.type !== 'Linux') throw new Error('Operasi sistem harus linux.');
-
 let exec = child.exec;
 
-// Validasi : Lavalink file
 
 let lava = require('./Lavalink.jar');
 if (lava === undefined) {
